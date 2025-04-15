ea6fffdd33440b5aee8a036d86ffa1af0a8759fc
Meirbek <mikospabekov2005@gmail.com>
2025-04-16 00:00:14 +0500

    ⚙️ Auto-load movies.json on Render

--- tickets/apps.py
+++ tickets/apps.py
@@ -1,6 +1,15 @@
+import os
 from django.apps import AppConfig
-
+from django.core.management import call_command
 
 class TicketsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tickets'
+
+    def ready(self):
+        if os.environ.get("RENDER"):
+            try:
+                call_command("loaddata", "movies.json")
+                print("✅ Fixtures loaded on Render.")
+            except Exception as e:
+                print(f"❌ Error loading fixtures: {e}")
